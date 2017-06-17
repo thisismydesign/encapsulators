c8f704ea83a475dfcb6a7aff43044d57ed46ae2a
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-18 00:21:02 +0200

    yes it does something useful

--- spec/encapsulators_spec.rb
+++ spec/encapsulators_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Encapsulators do
   it "has a version number" do
     expect(Encapsulators::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
