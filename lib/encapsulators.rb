ecda4362216ae82629f8c970e81265d3c57fbae0
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-18 00:27:44 +0200

    Add method references as attributes

--- lib/encapsulators.rb
+++ lib/encapsulators.rb
@@ -6,6 +6,8 @@ require_relative 'encapsulators/helpers'
 module Encapsulators
   include EasyLogging
 
+  attr_reader :exception_handling, :user_interaction_before_return, :lifecycle_signals
+
   def self.exception_handling(callback:, params: nil)
     begin
       ReflectionUtils.call(callback, params)
@@ -13,17 +15,20 @@ module Encapsulators
       logger.error(Helpers.ruby_style_trace(e))
     end
   end
+  @exception_handling = self.method(:exception_handling)
 
   def self.user_interaction_before_return(callback:, params: nil)
     ReflectionUtils.call(callback, params)
     logger.info('Press any key to continue')
     gets
   end
+  @user_interaction_before_return = self.method(:user_interaction_before_return)
 
   def self.lifecycle_signals(callback:, params: nil)
     logger.debug("Method '#{callback}' started")
     ReflectionUtils.call(callback, params)
     logger.debug("Method '#{callback}' finished")
   end
+  @lifecycle_signals = self.method(:lifecycle_signals)
 
 end
