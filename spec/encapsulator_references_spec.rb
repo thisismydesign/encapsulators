d87da2a5a0ccb9ea4d0f8d3d3ef1d72c21e25d09
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-21 19:05:48 +0200

    Add test with class that includes EncapsulatorReferences; Refactor existing EncapsulatorReferences test case

--- spec/encapsulator_references_spec.rb
+++ spec/encapsulator_references_spec.rb
@@ -2,9 +2,33 @@ require "spec_helper"
 
 RSpec.describe Encapsulators::EncapsulatorReferences do
 
-  it "contains method references corresponding to Encapsulators' methods" do
-    (Encapsulators.methods - Object.methods - [:logger]).each do |encapsulator|
+  let(:default_methods) {Object.methods + [:logger]}
+  let(:encapsulator_methods) {Encapsulators.methods - default_methods}
+
+  it "contains methods returning references to Encapsulators' methods" do
+    expect(Encapsulators::EncapsulatorReferences.methods).to include(*encapsulator_methods)
+
+    encapsulator_methods.each do |encapsulator|
       expect(Encapsulators::EncapsulatorReferences.method(encapsulator)).to be_a(Method)
+    end
+  end
+
+  context "class including EncapsulatorReferences" do
+
+    before :each do
+      class Something
+        include Encapsulators::EncapsulatorReferences
+      end
+    end
+
+    it "contains instance methods returning references to Encapsulators' methods" do
+      included_methods = Something.new.methods - default_methods
+      expect(encapsulator_methods).to include(*included_methods)
+    end
+
+    it "contains class methods returning references to Encapsulators' methods" do
+      included_methods = Something.new.methods - default_methods
+      expect(encapsulator_methods).to include(*included_methods)
     end
   end
 end
