448c3db1bc40059b41f015aa11a411fdeca0933f
Csaba Apagyi <csaba.apagyi@gmail.com>
2017-06-28 22:35:06 +0200

    Fix tests referring to renamed 'EncapsulatorReferences'

--- spec/encapsulator_references_spec.rb
+++ spec/encapsulator_references_spec.rb
@@ -1,23 +1,23 @@
 require "spec_helper"
 
-RSpec.describe Encapsulators::EncapsulatorReferences do
+RSpec.describe Encapsulators::References do
 
   let(:default_methods) {Object.methods + [:logger]}
   let(:encapsulator_methods) {Encapsulators.methods - default_methods}
 
   it "contains methods returning references to Encapsulators' methods" do
-    expect(Encapsulators::EncapsulatorReferences.methods).to include(*encapsulator_methods)
+    expect(Encapsulators::References.methods).to include(*encapsulator_methods)
 
     encapsulator_methods.each do |encapsulator|
-      expect(Encapsulators::EncapsulatorReferences.method(encapsulator)).to be_a(Method)
+      expect(Encapsulators::References.method(encapsulator)).to be_a(Method)
     end
   end
 
-  context "class including EncapsulatorReferences" do
+  context "class including References" do
 
     before :each do
       class Something
-        include Encapsulators::EncapsulatorReferences
+        include Encapsulators::References
       end
     end
 
